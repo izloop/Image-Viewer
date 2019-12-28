ca6669064438c4c17046073147c423a2fb4d373f
izloop <peter@ptip.uk>
2019-12-28 01:13:26 +0100

    Initial Commit

---
+++ ImgVue/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ImgVue
+//
+//  Created by Izloop on 12/28/19.
+//  Copyright © 2019 IPL. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
